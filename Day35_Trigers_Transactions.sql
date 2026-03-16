e2e57cb0f33948b19ba36607fea2dd940c4916e2
Navin Raj <navinraj.connect@gmail.com>
2026-03-16 23:11:28 +0530

    Add files via upload

---
+++ Day35_Trigers_Transactions.sql
@@ -0,0 +1,211 @@
+create database College;
+use college;
+create table students(stid int primary key auto_increment, stuname varchar(100) not null);
+create table courses(cid int primary key auto_increment, course_name varchar(100) not null)
+auto_increment=101;
+drop table courses;
+insert into students (stuname) values ('malini'), ('meghna'), ('meghala'),('priyanka'),('paluvenchekova');
+insert into courses (course_name) values ('jogging'),('running'),('mimicry'),('itching');
+create table enrollment (stid int, cid int);
+insert into enrollment values(1,101), (2,102), (3,101),(4,102),(5,103);
+select stuname,course_name
+from students as s
+inner join enrollment as e
+on s.stid=e.stid
+inner join courses as c
+on c.cid=e.cid;
+
+-- only students with valid enrollments included
+
+select stuname,course_name
+from students s
+left join enrollment e
+on s.stid=e.stid
+left join courses c
+on c.cid=e.cid;
+
+select stuname,course_name
+from courses c
+right join enrollment e
+on c.cid=e.cid
+right join students s
+on e.stid=s.stid;
+insert into students (stuname) values('pithambari');
+
+--  students/courses are shown with NULLs where no match exists
+
+select stuname as unions
+from students
+union 
+select course_name
+from courses;
+
+select stuname as unionsall
+from students
+union all 
+select course_name 
+from courses;
+
+-- UNION removes duplicates, UNION ALL keeps all rows executed 
+
+select stuname, upper(stuname) up from students;
+select stuname, lower(stuname) low from students;
+select stuname, substring(stuname, 1,5) from students;
+
+select stuname, concat(stuname, ' ', course_name) newname
+from students s
+join enrollment e
+on s.stid=e.stid
+join courses c
+on c.cid=e.cid;
+
+
+-- formatted names outputs are displayed
+
+alter table students add joiningdate date;
+update students set joiningdate='2020-02-01'  where stid=1;
+update students set joiningdate='2020-03-01'  where stid=2;
+update students set joiningdate='2020-02-04'  where stid=3;
+update students set joiningdate='2020-04-01'  where stid=4;
+update students set joiningdate='2021-06-30'  where stid=5;
+
+select stuname, year(joiningdate) tremendousjoin from students;
+select now();
+select stuname, year(now()) - year(joiningdate) tenure_years from students;
+select stuname, datediff(now(), joiningdate) Totaldays from students;
+ 
+ --- students tenure calculated correctly using year,datediff,now
+ 
+ alter table students add firstname varchar(100), add lastname varchar(100);
+ update students set firstname='namitha', lastname='waste'  where stid=1;
+ update students set firstname='tisha', lastname='ok'  where stid=2;
+ update students set firstname='federer', lastname='firstrank'  where stid=3;
+ update students set firstname='alugumunju', lastname='topcontender'  where stid=4;
+ update students set firstname='promise', lastname='thing'  where stid=5;
+ 
+ Delimiter $$
+ create function get_stuname (firstname varchar(100), lastname varchar(100))
+ returns varchar(200)
+ deterministic
+ begin
+ return concat(firstname, ' ' ,lastname);
+ End $$
+ Delimiter ;
+ 
+ select get_stuname(firstname, lastname) fullname from students;
+ 
+ --  Full name is returned when function is called.
+ 
+ select * from students;
+
+delimiter $$
+create procedure proceed (in sid int)
+begin
+select * from students where stid =sid;
+end $$
+delimiter ;
+call proceed(5);
+
+-- students details are returned when procedure is executed
+
+create view student_course_view as
+select stuname,course_name from students s
+join enrollment e
+on s.stid=e.stid
+join courses c
+on c.cid=e.cid;
+select * from student_course_view
+ 
+
+
+ -- new view created succesfully
+ 
+ 
+  create view students_multiple_columnview as
+  select s.stuname,c.course_name, c.cid, s.stid
+  from students s
+  join enrollment e
+  on s.stid=e.stid
+  join courses c
+  on e.cid=c.cid;
+ 
+ select * from students_multiple_columnview
+ 
+ -- view return combined datas from all tables
+ 
+ update students set stuname='roger federer' where stid=1;
+ update students set stuname='mirka federer' where stid=2;
+ update students set stuname='paluvenchekova' where stid=3;
+ update students set stuname='john arogiasamy' where stid=4;
+ update students set stuname='djokovic' where stid=5;
+ update students set stuname='stefanie graf' where stid=6;
+ show tables;
+ 
+ create table students_history(stid int, stuname varchar(100), deleted_time timestamp);
+
+delimiter $$
+create trigger log_student_delete
+before delete on students
+for each row
+begin
+insert into students_history(stid, stuname, deleted_time)
+values(old.stid, old.stuname, now());
+end$$
+delimiter ;
+
+delete from students where stid=4;
+
+select * from students_history;
+
+-- deleted records are logged automatically in students_history
+
+create user 'assistant_corruptist'@'%' 
+identified by 'people123';
+
+grant select on college.students to 'assistant_corruptist'@'%';
+
+revoke select on college.students from 'assistant_corruptist'@'%';
+
+-- User privileges are updated accordingly.
+
+start transaction;
+update students set stuname='mirnalini' where stid=4;
+savepoint sp1;
+rollback to sp1;
+commit;
+ 
+ --  Transaction integrity is maintained.
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
